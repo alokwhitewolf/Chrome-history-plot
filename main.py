2c95d5ac2d0a747301a11c167703eb499b3b98af
DESKTOP-VJ6R336\dell <kurdleak.alok@gmail.com>
2017-04-21 01:27:36 +0530

    Added parse(url)

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ import os
 
 
 
+def parse(url):
+	try:
+		parsed_url_components = url.split('//')
+		sublevel_split = parsed_url_components[1].split('/', 1)
+		domain = sublevel_split[0].replace("www.", "")
+		return domain
+	except IndexError:
+		print "Error reading URL"
+
+
 data_path = os.path.expanduser('~')+"\AppData\Local\Google\Chrome\User Data\Default" #path to user's history database (Chrome)
 files = os.listdir(data_path)
 
